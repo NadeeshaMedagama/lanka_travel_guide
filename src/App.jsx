888048c51f8a0fd38d2e0e882306784d1b898ac0
NadeeshaMedagama <nadeeshamedagama@gmail.com>
2026-06-05 13:21:54 +0530

    add App component with routing and main layout

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Detail from './pages/Detail/Detail';
+import Favorites from './pages/Favorites/Favorites';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/attraction/:id" element={<Detail />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
+  );
+}
+
+export default App;
